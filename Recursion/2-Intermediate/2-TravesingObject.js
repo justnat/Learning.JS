222856abb41a35da8c913e543006ad1b6462ec7d
kulinos <nathanielredilla@gmail.com>
2025-12-01 16:32:41 +0300

    Initial commit

---
+++ Recursion/2-Intermediate/2-TravesingObject.js
@@ -0,0 +1,26 @@
+let company = { // the same object, compressed for brevity
+      sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+      development: {
+          sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+          internals: [{name: 'Jack', salary: 1300}]
+      }
+  };
+
+function sumSalaries(department) {
+
+    if(Array.isArray(department)) {
+      return department.reduce((prev, curr) =>  prev + curr.salary ,0)
+    } else {
+      let sum = 0;
+      for(let obj of Object.values(department)) {
+      sum += sumSalaries(obj);
+      }
+    return sum;
+  }
+}
+
+
+const result = sumSalaries(company)
+
+console.log(result)
+
